b6fc8adb2af919020d296c6d495500f51792af1a
Petrushchenko <petrushchenko.l@ukr.net>
2018-06-19 00:14:33 +0300

    appearence of buttons for categories, sorting cats by category

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,8 @@
 'use strict'
 
 window.onload = function () {
-
-	var btnMore = getElem('.more', '.more__title')[0];
-	var dropdownMore = getElem('.more', '.more__items')[0];
-	var counter = 0;
 	
-	btnMore.addEventListener('click', function opening () {
-		
-		if(counter % 2 == 0) {
-			console.log(counter);
-			dropdownMore.classList.add("opened");
-			counter++;
-
-		} else {
-			dropdownMore.classList.remove('opened');
-			counter++;
-		}	
-	});
-	
+	modalMore();
 	
 	var gallery = document.querySelector('.gallery__items');
 	var kitty = document.querySelector('#template');
@@ -31,16 +15,27 @@ window.onload = function () {
 	var imagesList = getElem('.gallery__item', 'img');
 
 	bgColor(imagesList);
-
-	var kittyNames = getElem('.kitty__details', 'span:first-child');
-
-	var kittyCategory =  getElem('.kitty__details', 'span:last-child');
-
-	var kittyPrice =  getElem('.kitty__status', 'p > span');
-
 		
-	getCats('https://ma-cats-api.herokuapp.com/api/cats', imagesList, kittyNames, kittyCategory, kittyPrice);
+	getCats('https://ma-cats-api.herokuapp.com/api/cats', imagesList);
+
 	
+}
+function modalMore() {
+	var btnMore = getElem('.more', '.more__title')[0];
+	var dropdownMore = getElem('.more', '.more__items')[0];
+	var counter = 0;
+	btnMore.addEventListener('click', function () {
+		
+		if(counter % 2 == 0) {
+			console.log(counter);
+			dropdownMore.classList.add("opened");
+			counter++;
+
+		} else {
+			dropdownMore.classList.remove('opened');
+			counter++;
+		}	
+	});
 }
 
 function getElem (selector1, selector2) {
@@ -78,35 +73,16 @@ function enumeration (ar, n, j) {
 };
 
 
-function getCats(url, imgList, namesList, categoryList, priceList) {
+function getCats(url, imgList) {
 	var request = new XMLHttpRequest();
 
 	request.onload = function() {
 	  	if (request.status >= 200 && request.status < 400) {
 	  		
 		    var data = JSON.parse(request.responseText).cats;
-		    console.log(data);
-		    for (var i = 0; i < data.length; i++) {
+		    
+		    buildCats(data, imgList);
 
-		  		for (var key in data[i]) {
-				  	if (key == 'img_url') {
-
-						var img = imgList[i].removeAttribute('src');
-						img = imgList[i].setAttribute('src', data[i][key]);
-				  	}
-				  	if (key == 'name') {
-				  		namesList[i].textContent = data[i][key];
-				  	}
-				  	if (key == 'category') {
-				  		categoryList[i].textContent = data[i][key];
-				  	}
-				  	if (key == 'price') {
-				  		priceList[i].textContent = data[i][key];
-				  	}
-				}
-		    	
-		    }
-		   
 		} else {
 			alert('Ooops!');
 		    return;
@@ -118,4 +94,95 @@ function getCats(url, imgList, namesList, categoryList, priceList) {
 	request.send();
 };
 
+function buildCats(data, imgList) {
 
+	var namesList = getElem('.kitty__details', 'span:first-child');
+	var categoryList =  getElem('.kitty__details', 'span:last-child');
+	var priceList =  getElem('.kitty__status', 'p > span');
+	var obj = {};
+	for (var i = 0; i < data.length; i++) {
+
+  		for (var key in data[i]) {
+  			switch (key) {
+  				case 'img_url':
+	  				var img = imgList[i].removeAttribute('src');
+					img = imgList[i].setAttribute('src', data[i][key]);
+				break;
+
+				case 'name':
+					namesList[i].textContent = data[i][key];
+				break;
+
+				case 'category':
+					categoryList[i].textContent = data[i][key];
+			  		var str = data[i][key]; 		
+	    			obj[str] = true;
+	    			
+    			break;
+
+    			case 'price':
+    				priceList[i].textContent = data[i][key];
+    			break;
+  			}
+		  	
+		  
+		}
+    	
+    }
+    
+    var categories = Object.keys(obj)
+    console.log(categories);
+
+    var btnPlace = document.querySelector('.filter__item:last-child div');
+    cloneBtn(categories, btnPlace);
+    
+   	chooseCats(categoryList);
+
+   
+}
+
+function cloneBtn(arr, whereInsert) {
+
+	var btn = document.querySelector('.button-category');
+	for (var i = 0; i < arr.length ; i++) {
+
+		whereInsert.appendChild(btn.content.cloneNode(true));
+		whereInsert.children[i].textContent = arr[i];	
+	}
+}
+
+function chooseCats(arrOfElements) {
+	var btns = document.querySelectorAll('.filter__item:last-child div button');
+	btns.forEach(function (item){
+		var counter = 1;
+		item.counter = function () {
+			if (counter == 1) {
+				return counter +=1;
+			} else {return counter -=1;}
+		};
+
+		item.addEventListener('click', function(e) {
+			
+			item.style.color = '#9c9c9b';
+			if (counter == 1) {
+				arrOfElements.filter(function(el){
+			
+					if (el.textContent == item.textContent) {
+						el.parentNode.parentNode.parentNode.style.display = 'none';
+					}	
+				});
+			} else {
+				arrOfElements.filter(function(el){
+					if (el.textContent == item.textContent) {
+						el.parentNode.parentNode.parentNode.style.display = 'block';
+						item.style.color = '#2a2825';
+					} 
+				});
+				
+			}
+			item.counter();
+		});
+
+	});
+	
+}
